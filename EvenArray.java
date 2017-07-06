86c48141b8a70e8ebe007ed78a669f19235347af
vamshikrishna6avk <vamshikrishna6avk@gmail.com>
2017-07-06 16:55:45 +0530

    Add files via upload

---
+++ EvenArray.java
@@ -0,0 +1,19 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class EvenArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int k[] = new int[a];
+		for (int i=1;i<=k.length;i++){
+			if(i%2==0){
+				System.out.println(i);
+			}
+			}
+		
+	}
+
+}
